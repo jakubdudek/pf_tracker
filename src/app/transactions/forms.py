ea839884aaf1ad9d2360a57b03f0692087930fe7
Jakub Dudek <jakubdudek@mac.com>
2015-06-12 13:46:33 -0400

    added custom validators

--- src/app/transactions/forms.py
+++ src/app/transactions/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField, StringField, SelectField, DateField, DecimalField
-from wtforms.validators import Required, Length
+from wtforms.validators import Required, Length, ValidationError
 
 class UploadForm(Form):
     transactions = FileField(validators=[FileRequired('Please chose a file!'), FileAllowed(['csv', 'CSV'], 'CSV only!')])
@@ -9,12 +9,28 @@ class UploadForm(Form):
 
 #class NewTransaction(Form):
 
+def date_check(form, field):
+    import datetime
+    
+    try:
+        datetime.datetime.strptime(field.data, '%Y-%m-%d')
+    except ValueError:
+        raise ValidationError("Incorrect data format, should be YYYY-MM-DD")
+
+def ticker_check(form, field):
+    import pandas.io.data as web
+    
+    if(field.data != "MYCASH"):
+        try:
+            quote=web.DataReader(field.data, 'yahoo', datetime.today())
+        except:
+            raise ValidationError("Invalid stock ticker")
 
 class NewTransaction(Form):
-    choices=[("BUY","BUY"), ("SELL", "SELL"), ("SPLIT", "SPLIT"), ("DIVIDEND", "DIVIDEND")]
-    date = StringField('Date', validators=[Required()])
+    choices=[("BUY","BUY"), ("SELL", "SELL"), ("SPLIT", "SPLIT"), ("DIVIDEND", "DIVIDEND") , ("DEPOSIT", "DEPOSIT")]
+    date = StringField('Date', validators=[Required(), date_check])
     trade = SelectField(u'Trade', choices=choices)
-    symbol = StringField('Symbol', validators=[Required()])
+    symbol = StringField('Symbol', validators=[Required(), ticker_check])
     shares = StringField('Shares', validators=[Required()])
     price = StringField('Price', validators=[Required()])
     comission = StringField('Comission', validators=[Required()])
