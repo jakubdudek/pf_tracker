e4669c907fcdac32397546f7a4235f6d66174508
Jakub Dudek <jakubdudek@mac.com>
2015-07-23 11:43:50 -0400

    removing automatic submit field

--- src/app/auth/forms.py
+++ src/app/auth/forms.py
@@ -7,11 +7,9 @@ class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
-    #submit = SubmitField('Log In')
 
 class RegisterForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     name = StringField('Name', validators=[Required(), Length(1, 64)])
     password = PasswordField('Password', validators=[Required()])
     password2 = PasswordField('Password', validators=[Required()])
-    #submit = SubmitField('Register')
